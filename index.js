53ebdfb2b02b21dd6c6ab28ec582790403da2693
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-09-18 12:25:27 -0400

    static methods lab done'

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  }
+  static titleize(string) {
+    const exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    let words = string.split(" ");
+    words = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        word = this.capitalize(word);
+      }
+      return word;
+    });
+    return words.join(" ");
+  }
 }
